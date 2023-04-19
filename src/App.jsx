017671680f58f4d74a1a3411d31fa9fd2d8a49bb
Dan <35927536+dan-mba@users.noreply.github.com>
2023-04-19 08:12:10 -0400

    initial migration to react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,71 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import ReactPlayer from 'react-player/file';
 import Podcasts from './Podcasts';
 import './App.css';
 
 export default function App() {
-  let [rss, setRss] = useState(undefined);
-  let [updating, setUpdating] = useState(false);
   let [url, setUrl] = useState('');
   let [selected, setSelected] = useState(-1);
-    
+  
+  const { isLoading, error, data: {rss} } = useQuery({
+    queryKey: ["espnPodcasts"],
+    queryFn: async () =>  {
+      const data = await fetch('https://flannel-glade.glitch.me/?' + new URLSearchParams({
+        rss: 'http://www.espn.com/espnradio/feeds/rss/podcast.xml?id=2942325'
+      }).toString())
+      const rss = await data.json();
+      return rss;
+    }
+  });
+
   function handleClick(url, selectedItem) {
     setUrl(url)
     setSelected(selectedItem)
   }
 
-  /* Retrieve the JSON version of the RSS feed from the back-end & overcome CORS  */
-  useEffect(() => {
-    async function fetchData() {
-      const data = await fetch('https://flannel-glade.glitch.me/?' + new URLSearchParams({
-        rss: 'http://www.espn.com/espnradio/feeds/rss/podcast.xml?id=2942325'
-      }).toString());
-      const json = await data.json();
-      localStorage.setItem('espn-podcasts', JSON.stringify(json.rss));
-      setRss(json.rss);
-      setUpdating(false);
-    }
+  if (error) {
+    return <div>{error}</div>
+  }
 
-    const podcasts = localStorage.getItem('espn-podcasts');
-    if (podcasts) {
-      setRss(JSON.parse(podcasts));
-      setUpdating(true);
-    }
-
-    fetchData()
-      .catch(console.error);
-  }, []);
-
-  if (!rss) {
+  if (isLoading) {
     return (
       <div className="App">
         <div>Loading</div>
         <div className="lds-facebook"><div></div><div></div><div></div></div>
       </div>
     );
-  } else {
-    return (
-      <div className="App">
-        <div className="Header">
-          <img src={rss.channel.image.url} alt=""></img>
-          <div className="HeaderText">
-            <div className="Title">{rss.channel.title}</div>
-            <div>{rss.channel.description}</div>
-          </div>
-        </div>
-
-        <ReactPlayer className="mediaPlayer" url={url} 
-          height="40px" width="100%"
-          config={{
-            forceAudio: true,
-            attributes: {
-              height: "40px",
-              width: "100%",
-              controls: true
-            },
-            tracks: []
-          }}
-        />
-
-        {updating ? <div className="updating">Using cached podcast feed</div> : null}
-
-        <Podcasts items={rss.channel.item} 
-          clickFunc={handleClick}
-          selected={selected}/>
-      </div>
-    );
   }
+
+  return (
+    <div className="App">
+      <div className="Header">
+        <img src={rss.channel.image.url} alt=""></img>
+        <div className="HeaderText">
+          <div className="Title">{rss.channel.title}</div>
+          <div>{rss.channel.description}</div>
+        </div>
+      </div>
+
+      <ReactPlayer className="mediaPlayer" url={url} 
+        height="40px" width="100%"
+        config={{
+          forceAudio: true,
+          attributes: {
+            height: "40px",
+            width: "100%",
+            controls: true
+          },
+          tracks: []
+        }}
+      />
+
+      {/*updating ? <div className="updating">Using cached podcast feed</div> : null*/}
+
+      <Podcasts items={rss.channel.item} 
+        clickFunc={handleClick}
+        selected={selected}/>
+    </div>
+  );
+
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css';
 import App from './App';
+
+const queryClient = new QueryClient();
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 );
