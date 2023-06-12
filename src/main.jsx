6fe47a677b855ae92afa14738091e7f68428a922
Dan <35927536+dan-mba@users.noreply.github.com>
2023-06-12 06:13:20 -0400

    Migrate to tanstack/react-query for data fetching (#163)

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,38 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { persistQueryClient, removeOldestQuery } from '@tanstack/react-query-persist-client';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
 import './index.css';
 import App from './App';
+
+const THIRTY_DAYS = 1000 * 60 * 60 * 24 * 30
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: THIRTY_DAYS,
+    },
+  },
+});
+
+const localStoragePersister = createSyncStoragePersister({
+  storage: window.localStorage,
+  key: 'ESPN_PODCAST_FEED',
+  retry: removeOldestQuery
+});
+
+
+persistQueryClient({
+  queryClient,
+  persister: localStoragePersister,
+  maxAge: THIRTY_DAYS
+})
+
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-  <StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </StrictMode>,
+  </QueryClientProvider>
 );
